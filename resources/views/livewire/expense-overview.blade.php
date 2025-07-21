6e1fb364854f7c4d83bcfd8360458afb0a83a1bf
Madrigal-Carl <carlsalido.madrigal@gmail.com>
2025-07-21 20:35:13 +0800

    change design in expense overview when empty

--- resources/views/livewire/expense-overview.blade.php
+++ resources/views/livewire/expense-overview.blade.php
@@ -10,17 +10,14 @@
         <div class="flex flex-col items-center justify-center p-6 text-[#BDBEC3] gap-1">
             <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="none"
                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                class="lucide lucide-calendar-range-icon lucide-calendar-range">
-                <rect width="18" height="18" x="3" y="4" rx="2" />
-                <path d="M16 2v4" />
-                <path d="M3 10h18" />
-                <path d="M8 2v4" />
-                <path d="M17 14h-6" />
-                <path d="M13 18H7" />
-                <path d="M7 14h.01" />
-                <path d="M17 18h.01" />
+                class="lucide lucide-file-text-icon lucide-file-text">
+                <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z" />
+                <path d="M14 2v4a2 2 0 0 0 2 2h4" />
+                <path d="M10 9H8" />
+                <path d="M16 13H8" />
+                <path d="M16 17H8" />
             </svg>
-            <p class="text-xs mt-1">No Orders Available</p>
+            <p class="text-xs mt-1">No Expenses Available</p>
         </div>
     @endforelse
     @if ($expenses->count() >= $take)
